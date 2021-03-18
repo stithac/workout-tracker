861b256220fddfc488decf1c3bf0c7cc326ca50b
Ashley Stith <ashleyc.stith@gmail.com>
2021-03-18 16:51:31 -0400

    deploying

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    Exercise: require("../models/Exercise.js"),
-    Workout: require("../models/Workout.js"),
+    Exercise: require("/models/Exercise.js"),
+    Workout: require("/models/Workout.js"),
   };
